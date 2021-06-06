fbf0aa29d40e81b4309ad498276a1d1b811d17de
Benfany Aditia <56857771+benfany505@users.noreply.github.com>
2021-06-06 22:48:52 +0700

    Keypad and LCD

--- arduino_lcd/src/main.cpp
+++ arduino_lcd/src/main.cpp
@@ -1,29 +1,35 @@
 #include <Arduino.h>
 #include <Wire.h>
 #include <LiquidCrystal.h>
+#include <Keypad.h>
 
 const int rs = 12, e = 11, d4 = 5, d5 = 4, d6 = 3, d7 = 2;
 LiquidCrystal lcd(rs, e, d4, d5, d6, d7);
 
-int i=0;
+const int ROW_NUM = 4;
+const int COL_NUM = 3;
+
+char keys[ROW_NUM][COL_NUM] = {
+    {'A', 'B', 'C'},
+    {'4', '5', '6'},
+    {'7', '8', '9'},
+    {'*', '0', '#'},
+};
+
+// Deklarasi koneksi ke mikro
+byte pin_rows[ROW_NUM] = {7, 6, 1, 0};
+byte pin_column[COL_NUM] = {8, 9, 10};
+
+Keypad keypad = Keypad(makeKeymap(keys), pin_rows, pin_column, ROW_NUM, COL_NUM);
 
 void setup()
 {
   // put your setup code here, to run once:
   lcd.begin(16, 2);
-  lcd.print("HobtechTV");
-  
 }
 
 void loop()
 {
-  // put your main code here, to run repeatedly:
-  lcd.scrollDisplayRight();
-  i++;
-  delay(100);
-  if(i>6){
-    i=0;
-    lcd.setCursor(0,1);
-    lcd.print("HobtechTV");
-  }
+  char key = keypad.waitForKey();
+  lcd.print(key);
 }
